1e70229d699046f52bd9bd7d89ca1410f02b97dd
mh5986759 <mh5986759@gmail.com>
2017-05-30 01:40:55 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-redis_db = 0 # Set your redis db number 0 - 15
-token = "SET_YOUR_TOKEN_HERE"
-sudos = []
-admins = []
-errors_chat =  #Set your errors chat(group/channel/user) id here!
-log_chat =  #Set your logs chat(group/supergroup) id here!
+redis_db = 15 # Set your redis db number 0 - 15
+token = "275649365:AAHBkmyUUZXY7iusQ5r-mzqj4TiOCh8FQ3U"
+sudos = [342293523]
+admins = [342293523]
+errors_chat -1001135307194  #Set your errors chat(group/channel/user) id here!
+log_chat = -1001135307194 #Set your logs chat(group/supergroup) id here!
